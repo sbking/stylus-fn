2a9f0c058845418f97ba5d74527832055b489bfb
Stephen King <stephen.brian.king@gmail.com>
2014-01-10 08:55:44 -0800

    Improve test output

--- test/runner.js
+++ test/runner.js
@@ -4,8 +4,9 @@ var fs = require("fs"),
   should = require("chai").should(),
   css = require("css"),
   stylus = require("stylus"),
+  _ = require("underscore"),
   fn = require("../"),
-  tests;
+  tests, categories;
 
 css.normalize = function(path) {
   return css.stringify(css.parse(path), {
@@ -13,30 +14,39 @@ css.normalize = function(path) {
   });
 };
 
-tests = glob.sync("test/cases/**/*.styl").map(function(filePath) {
+categories = _.groupBy(glob.sync("test/cases/**/*.styl").map(function(filePath) {
   return {
     name: path.basename(filePath, ".styl"),
     path: path.dirname(filePath)
   };
+}), function(test) {
+  return _.last(test.path.split(path.sep));
 });
 
 describe("fn", function() {
-  tests.forEach(function(test) {
-    var name = test.name.replace(/[-.]/g, " ");
-    it(name, function() {
-      var stylPath = path.join(test.path, test.name + ".styl"),
-        cssPath = path.join(test.path, test.name + ".css"),
-        expected = css.normalize(fs.readFileSync(cssPath, "utf8"));
+  _.each(categories, function(tests, category) {
 
-      stylus(fs.readFileSync(stylPath, "utf8"))
-        .use(fn)
-        .set("filename", stylPath)
-        .define("url", stylus.url())
-        .render(function(err, compiled) {
-          if (err) throw err;
-          var actual = css.normalize(compiled);
-          actual.should.equal(expected);
+    describe(category, function() {
+      _.each(tests, function(test) {
+
+        it(test.name, function() {
+          var stylPath = path.join(test.path, test.name + ".styl"),
+            cssPath = path.join(test.path, test.name + ".css"),
+            expected = css.normalize(fs.readFileSync(cssPath, "utf8"));
+
+          stylus(fs.readFileSync(stylPath, "utf8"))
+            .use(fn)
+            .set("filename", stylPath)
+            .define("url", stylus.url())
+            .render(function(err, compiled) {
+              if (err) throw err;
+              var actual = css.normalize(compiled);
+              actual.should.equal(expected);
+            });
         });
+
+      });
     });
+
   });
 });
